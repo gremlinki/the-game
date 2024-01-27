cd5b1c4b5ba3d5b77b328f3bfad64f703828aeff
QwertyTheProGrammar <kwertypoi123456789@gmail.com>
2024-01-27 21:06:48 +0100

    FIrst prototype of map ready
    Lightining + enitre map done (some bug fixes soon)

--- Assets/Scripts/scr_Torch.cs
+++ Assets/Scripts/scr_Torch.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+
+public class scr_Torch : MonoBehaviour
 {
+
+    Transform mainLight;
+    Transform flickerLight;
+    UnityEngine.Rendering.Universal.Light2D mainLightComponent;
+    UnityEngine.Rendering.Universal.Light2D flickerLightComponent;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        flickerLightComponent = this.GetComponent<UnityEngine.Rendering.Universal.Light2D>();
+
+        StartCoroutine(Timer());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Timer()
     {
-        
+        for (; ; ) //this is while(true)
+        {
+            float randomIntensity = Random.Range(0.5f, 0.55f);
+            flickerLightComponent.intensity = randomIntensity;
+
+
+            float randomTime = Random.Range(0.09f, 0.14f);
+            yield return new WaitForSeconds(randomTime);
+        }
     }
 }
