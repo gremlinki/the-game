d53ffce83740bea980adf51ecc9380d9e0ebe333
QwertyTheProGrammar <kwertypoi123456789@gmail.com>
2024-01-27 21:58:39 +0100

    ugh uhhhh
    another map update !

--- Assets/Scripts/scr_Torch.cs
+++ Assets/Scripts/scr_Torch.cs
@@ -3,32 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class scr_Torch : MonoBehaviour
-{
+public class scr_Torch : MonoBehaviour{
 
-    Transform mainLight;
     Transform flickerLight;
-    UnityEngine.Rendering.Universal.Light2D mainLightComponent;
     UnityEngine.Rendering.Universal.Light2D flickerLightComponent;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        flickerLightComponent = this.GetComponent<UnityEngine.Rendering.Universal.Light2D>();
+    void Start(){
+        flickerLightComponent = flickerLight.GetComponent<UnityEngine.Rendering.Universal.Light2D>();
 
         StartCoroutine(Timer());
     }
 
-    IEnumerator Timer()
-    {
-        for (; ; ) //this is while(true)
-        {
-            float randomIntensity = Random.Range(0.4f, 0.55f);
+    IEnumerator Timer(){
+        for (; ; ){
+            float randomIntensity = Random.Range(0.45f, 0.55f);
             flickerLightComponent.intensity = randomIntensity;
-
-
-            float randomTime = Random.Range(0.09f, 0.14f);
+            float randomTime = Random.Range(0.11f, 0.17f);
             yield return new WaitForSeconds(randomTime);
         }
     }
